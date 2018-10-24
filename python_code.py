6d12133a81e92f69d80484184c1ebc6ee05986d0
skrzyp1984@o2.pl <skrzyp1984@o2.pl>
2018-10-24 16:21:29 +0200

    model - DecisionTreeClassifier()

--- python_code.py
+++ python_code.py
@@ -83,3 +83,46 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 y_train
 print("Train:", X_train.shape, y_train.shape)
 print("Test:", X_test.shape, y_test.shape)
+
+train_and_predict_model(X_train, X_test, y_train, y_test, LogisticRegression())
+
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+
+def plot_learning_curve(model, title, X, y, ylim=None, cv=None, n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    
+    plt.figure(figsize=(12,8))
+    plt.title(title)
+    if ylim is not None:plt.ylim(*ylim)
+
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    
+    train_sizes, train_scores, test_scores = learning_curve(
+        model, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.grid()
+
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+
+    plt.legend(loc="best")
+    return plt
+
+	
+plt = plot_learning_curve(LogisticRegression(), "Learning Curves (Logistic Regression)", X, y, ylim=(0.7, 1.02), cv=10, n_jobs=4)
+plt.show()
+
+train_and_predict_model(X_train, X_test, y_train, y_test, DecisionTreeClassifier())
+plt = plot_learning_curve(DecisionTreeClassifier(), "Learning Curves (Decision Tree)", X, y, ylim=(0.7, 1.02), cv=10)
+plt.show()
